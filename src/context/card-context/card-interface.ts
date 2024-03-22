c8ef3c2ec4e6cf33b7f21d72ee1591412fb7d048
Faraz Khan <farazkhanaa2555@gmail.com>
2024-03-22 19:37:15 +0500

    refactor: updated code with some changes

--- src/context/card-context/card-interface.ts
+++ src/context/card-context/card-interface.ts
@@ -32,7 +32,7 @@ export const CardDefaultValue: CardInterface = {
     throw new Error('Function not implemented.')
   },
   moveCardToNextColumn: function (): void {
-    throw new Error('Function not implemented.')
+    throw new Error('Function not implemented')
   },
   moveCardToPreviousColumn: function (): void {
     throw new Error('Function not implemented.')
